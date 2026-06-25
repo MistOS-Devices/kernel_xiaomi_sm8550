0f1ea22dc9047c5af3bb1a5350ade67b308e099f
Lee Jones <joneslee@google.com>
2026-06-25 11:59:55 +0100

    ANDROID: Fix ABI issues related to changed hid_report_raw_event()
    Upstream recently started passing the original buffer size through
    hid_report_raw_event() in order to fix a few recently reported
    vulnerabilities.  These issues also affect Android so we need this
    change.  However, since it changes the function signature which breaks
    ABI, we need to provide a wrapper.
    
    Bug: 483018448
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: I2c50f89815a713391a6ae9510601a3a0ebfc4d5e
    (cherry picked from commit 26ded8a3ca6ea3bc0030bbbd3e5ed0c8c6e37484)

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -1769,8 +1769,8 @@ out:
 }
 EXPORT_SYMBOL_GPL(__hid_request);
 
-int hid_report_raw_event(struct hid_device *hid, int type, u8 *data,
-			 size_t bufsize, u32 size, int interrupt)
+int __hid_report_raw_event(struct hid_device *hid, int type, u8 *data,
+			   size_t bufsize, u32 size, int interrupt)
 {
 	struct hid_report_enum *report_enum = hid->report_enum + type;
 	struct hid_report *report;
@@ -1912,7 +1912,7 @@ int hid_input_report(struct hid_device *hid, int type, u8 *data, u32 size, int i
 			goto unlock;
 	}
 
-	ret = hid_report_raw_event(hid, type, data, bufsize, size, interrupt);
+	ret = __hid_report_raw_event(hid, type, data, bufsize, size, interrupt);
 
 unlock:
 	up(&hid->driver_input_lock);
